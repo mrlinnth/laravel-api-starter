2278e81497bf79d9ed1273ffb17a1dd1e97b862e
Yan Linn Thor <2441118-yanlinnthor@users.noreply.gitlab.com>
2021-11-25 11:33:41 +0630

    Install laravel-debug-bar

--- app/Http/Controllers/MongoUserController.php
+++ app/Http/Controllers/MongoUserController.php
@@ -3,8 +3,29 @@
 namespace App\Http\Controllers;
 
 use App\Models\MongoUser;
+use Illuminate\Support\Facades\Hash;
 
 class MongoUserController extends Controller
 {
     public static $model = MongoUser::class;
+
+    /**
+     * Check existing MongoDB password hash
+     * @return array
+     */
+    public function checkPassword(): array
+    {
+        $model = new static::$model;
+        $resource = $model::with($model::getItemWith())->where('email', '=', 'admin@admin.com')->first();
+        $data = 'not match';
+
+        if (Hash::check('password', $resource->password)) {
+            $data = 'password match';
+        }
+
+        return [
+            'data' => $data,
+            'resource' => $resource
+        ];
+    }
 }
--- app/Http/Controllers/PlayGroundController.php
+++ app/Http/Controllers/PlayGroundController.php
@@ -2,26 +2,13 @@
 
 namespace App\Http\Controllers;
 
-use App\Models\MongoUser;
-use Illuminate\Support\Facades\Hash;
-
 class PlayGroundController extends Controller
 {
-    public static $model = MongoUser::class;
 
     public function index(): array
     {
-        $model = new static::$model;
-        $resource = $model::with($model::getItemWith())->where('email', '=', 'uatmerchanttest1@yopmail.com')->first();
-        $data = 'not match';
-
-        if (Hash::check('qwer@1234', $resource->password)) {
-            $data = 'password match';
-        }
-
         return [
-            'data' => $data,
-            'resource' => $resource
+            'data' => 'Hello',
         ];
     }
 
